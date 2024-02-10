779a527d7f5b2f0f64280323717baa99c0688856
prakash kumar samantaray <psamantaray77@gmail.com>
2024-02-10 18:20:04 +0530

    chnages fornncmfmf

--- keys/development.keys.js
+++ keys/development.keys.js
@@ -6,7 +6,7 @@ module.exports = {
     'PASSWORD_FORM': 'ezTruck@3579',
     'EMAIL_SERVICE': 'gmail',
     'MONGODB_URI': 'mongodb+srv://root:akki909@cluster0.sm3rshd.mongodb.net/Bhakati',
-    'BASEURL': 'http://13.51.106.139:8001',
+    'BASEURL': 'http://16.170.253.177:8001',
     'APPID':'064f804292cf437d8d949e9e1ed01d3e',
     'APPCERTIFICATE':'e6b6368d981c47b3bb99ad3a5ae1e4cd',
     'MUX_TOKEN_ID':'108b7df9-6871-4585-91a1-1e567afb3930',
