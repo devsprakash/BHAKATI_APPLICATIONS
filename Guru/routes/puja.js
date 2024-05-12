e9f71e7b11176a8771b8fe7b18a4ebaefcf9a36c
Tusarkant Sahoo <trytusar@gmail.com>
2024-05-12 09:43:21 +0530

    path

--- Guru/routes/puja.js
+++ Guru/routes/puja.js
@@ -11,7 +11,8 @@ const { add_puja_validator, Validator_Result, puja_add_by_temple_validator, dele
 router.post('/addNewPuja', upload.single('puja_image'), add_puja_validator, Validator_Result, authenticate, addNewPuja);
 router.get('/getAllpuja', authenticate, getAllPuja);
 router.post('/addpuja', puja_add_by_temple_validator, Validator_Result, TempleAuth, addPuja);
-router.get('/MasterPuja', TempleAuth, ListOfPuja)
+//router.get('/MasterPuja', TempleAuth, ListOfPuja)
+router.get('/MasterPuja', ListOfPuja)
 router.get('/TempleUnderAllpujaList', TempleAuth, pujs_by_temple)
 router.get('/templeUnderAllpujaList/:temple_id', authenticate, temple_under_puja_list)
 router.delete('/deletePuja', delete_puja_validator, Validator_Result, TempleAuth, deletePuja);
